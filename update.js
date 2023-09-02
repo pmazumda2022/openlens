593209fe8253ed55b8cde111f6c7452591258b12
pinakmazumdar <mazumdar.pinak@gmail.com>
2023-09-02 22:13:06 +0530

    update scriptS

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-var packagejson = require('./package.json');
+const packagejson = require('./lens/open-lens/package.json');
 
 packagejson.build.publish = [{
 	url: "https://github.com/pmazumda2022/openlens/releases/download/Latest",
@@ -8,5 +8,22 @@ packagejson.build.publish = [{
 }];
 
 packagejson.build.win.artifactName = "OpenLens.Setup.${version}.${ext}";
+packagejson.version = `${packagejson.version}-${process.env.BUILD_ID}`;
+packagejson.build.detectUpdateChannel = false;
 
-fs.writeFileSync('package.json', JSON.stringify(packagejson));
+delete packageJsonOpenLens.scripts.postinstall;
+
+packagejson.copyright = [
+    packagejson.copyright,
+    '',
+    'OpenLens Windows Binary builds @ pmazumda2022/openlens',
+    'by Pinak Mazumdar'
+]
+
+
+
+console.log(`Set build version: ${packagejson.version}`)
+
+
+
+fs.writeFileSync('./lens/open-lens/package.json', JSON.stringify(packagejson, null, 2));
